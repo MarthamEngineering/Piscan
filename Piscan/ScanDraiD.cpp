88a752cabd8680fef61bc771d2acf893e7015809
Daniel <s.d.wood.82@googlemail.com>
2014-01-26 08:54:24 +0000

    Updated license and relicensed LGPL to GPL

--- Piscan/ScanDraiD.cpp
+++ Piscan/ScanDraiD.cpp
@@ -1,21 +1,27 @@
 /*
-* This program is free software; you can redistribute it and/or modify
-* it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE Version 3 as published by
-* the Free Software Foundation.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU LESSER GENERAL PUBLIC LICENSE Version 3 for more details.
-*
-* Changelog:
-*
-* 2007 The original source was created by Steve Baker:
-*    http://www.sjbaker.org/wiki/index.php?title=A_Simple_3D_Scanner
-* 2009-02-20 <a.w@quirxi.net>:
-*   Rewrite of code so that it can be used as a command line tool.
-* 2014-01-05 <dwood@marthamengineering.co.uk>
-*   Added PLY file output
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+ Changelog:
+
+ 2007 The original source was created by Steve Baker:
+    http://www.sjbaker.org/wiki/index.php?title=A_Simple_3D_Scanner
+ 2009-02-20 <a.w@quirxi.net>:
+    Rewrite of code so that it can be used as a command line tool.
+ 2014-01-05 <dwood@marthamengineering.co.uk>
+    -Relicensed from LGPL to GPL
+    -Added PLY file output
+    -Converted ScanDraid to a Qt class to use signal and slots
 */
 
 #include <QDebug>
--- README.md
+++ README.md
@@ -1,4 +1,24 @@
-Piscan
-======
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE Version 3 as published by
+ the Free Software Foundation.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU LESSER GENERAL PUBLIC LICENSE Version 3 for more details.
 
-3D Scanning Software for use with opensource Piscan Hardware
+================================================================================
+Piscan by Martham Engineering (www.Marthamengineering.co.uk)
+================================================================================
+Piscan is 3D scanning software written in C++ using the Qt framework (http://qt.digia.com/) 
+and designed to work with the Piscan hardware https://github.com/MarthamEngineering/Piscan-Hardware 
+
+Piscan is based on sjbakers 'A Simple 3D Scanner' http://www.sjbaker.org/wiki/index.php?title=A_Simple_3D_Scanner and ScanDraid https://launchpad.net/scandraid. 
+
+
+
+================================================================================
+
+The current state is "Pre-Alpha", more of a proof of concept and it is not ready for end users. 
+Feature requests, pull requests, bugfixes and bug reports are welcomed. 
+
+================================================================================
